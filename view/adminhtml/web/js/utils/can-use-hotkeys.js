481af565c8081e04cb60b5b4089f737b87c8a296
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-02-23 20:37:22 +0200

    Do not block "copy" hotkey when event.target comes from closed modal

--- view/adminhtml/web/js/utils/can-use-hotkeys.js
+++ view/adminhtml/web/js/utils/can-use-hotkeys.js
@@ -8,6 +8,11 @@ define([
             return false;
         }
 
+        var modal = $(event.target).closest('[data-role="modal"]');
+        if (modal.length && !modal.hasClass('_show')) {
+            return true;
+        }
+
         if ($(event.target).is('input, select, textarea') && $(event.target).css('clip') === 'auto') {
             if (!allowedInputs || allowedInputs && !$(event.target).is(allowedInputs)) {
                 return false
