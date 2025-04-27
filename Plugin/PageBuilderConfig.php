78d53e9be241af1abb0a4f87c7b64e87028e8502
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-04-27 18:21:22 +0300

    Additional check for component name

--- Plugin/PageBuilderConfig.php
+++ Plugin/PageBuilderConfig.php
@@ -7,13 +7,17 @@ use Magento\PageBuilder\Model\ConfigInterface;
 class PageBuilderConfig
 {
     /**
-     * This code makes Magento to send "*_mobile_form" render request when editing
-     * pagebuilder component in mobile view.
+     * This code makes Magento to send "render/*_mobile_form" request when editing
+     * pagebuilder component in mobile view even if there is no *_mobile_form.xml file
      */
     public function afterGetContentTypes(ConfigInterface $subject, array $result)
     {
         foreach ($result as &$contentType) {
-            if (!isset($contentType['breakpoints']) && isset($contentType['form'])) {
+            if (!isset($contentType['breakpoints']) &&
+                isset($contentType['form']) &&
+                str_starts_with($contentType['form'], 'pagebuilder_') &&
+                str_ends_with($contentType['form'], '_form')
+            ) {
                 $contentType['breakpoints'] = [
                     'mobile' => [
                         'form' => str_replace('_form', '_mobile_form', $contentType['form']),
