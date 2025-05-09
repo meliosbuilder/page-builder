044b8ff2d003b4f10353f1612b4dbfa1043f0ac8
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-05-09 20:11:20 +0300

    WIP

--- view/adminhtml/web/js/image/form/dimensions.js
+++ view/adminhtml/web/js/image/form/dimensions.js
@@ -60,34 +60,52 @@ define([
                 checkbox.checked(true);
             });
 
+            uiRegistry.get(this.paths['width'], input => {
+                input.on('disabled', () => {
+                    this.aspectRatio = 0;
+                });
+            });
+
             return this;
         },
 
         onImageChange: function (image) {
             ko.getObservable(image[0], 'previewWidth')?.subscribe(width => {
-                setTimeout(() => {
-                    var newRatio = image[0].previewWidth / (image[0].previewHeight || 1),
-                        oldRatio = this.width() / (this.height() || 1);
+                uiRegistry.get(this.paths['width'], input => {
+                    setTimeout(() => {
+                        var newRatio = image[0].previewWidth / (image[0].previewHeight || 1),
+                            oldRatio = this.width() / (this.height() || 1);
 
-                    this.aspectRatio = newRatio;
-                    if (newRatio === oldRatio) {
-                        return;
-                    }
+                        if (input.disabled() && newRatio !== oldRatio) {
+                            uiRegistry.get(`${this.name}.use_mobile_dimensions`).checked(true);
+                        }
 
-                    this.updateSizeIgnoringAspectRatio('width', width / 2);
+                        this.aspectRatio = newRatio;
+                        if (newRatio === oldRatio) {
+                            return;
+                        }
+
+                        this.updateSizeIgnoringAspectRatio('width', width / 2);
+                    });
                 });
             });
             ko.getObservable(image[0], 'previewHeight')?.subscribe(height => {
-                setTimeout(() => {
-                    var newRatio = image[0].previewWidth / (image[0].previewHeight || 1),
-                        oldRatio = this.width() / (this.height() || 1);
+                uiRegistry.get(this.paths['height'], input => {
+                    setTimeout(() => {
+                        var newRatio = image[0].previewWidth / (image[0].previewHeight || 1),
+                            oldRatio = this.width() / (this.height() || 1);
 
-                    this.aspectRatio = newRatio;
-                    if (newRatio === oldRatio) {
-                        return;
-                    }
+                        if (input.disabled() && newRatio !== oldRatio) {
+                            uiRegistry.get(`${this.name}.use_mobile_dimensions`).checked(true);
+                        }
 
-                    this.updateSizeIgnoringAspectRatio('height', height / 2);
+                        this.aspectRatio = newRatio;
+                        if (newRatio === oldRatio) {
+                            return;
+                        }
+
+                        this.updateSizeIgnoringAspectRatio('height', height / 2);
+                    });
                 });
             });
         },
