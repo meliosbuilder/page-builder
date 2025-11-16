28e39d125bbaaca16449aec5ed53f0fbc9977c8d
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-11-16 23:59:36 +0200

    Fixed possible missing dynamic elements in spotlight popup

--- view/adminhtml/web/js/spotlight/page-builder-mixin.js
+++ view/adminhtml/web/js/spotlight/page-builder-mixin.js
@@ -1,5 +1,6 @@
 define([
     'jquery',
+    'underscore',
     'ko',
     'mage/utils/wrapper',
     'Magento_PageBuilder/js/events',
@@ -7,7 +8,7 @@ define([
     'Melios_PageBuilder/js/spotlight/spotlight',
     'Melios_PageBuilder/js/utils/can-use-hotkeys',
     'Melios_PageBuilder/js/utils/is-in-viewport'
-], function ($, ko, wrapper, events, matrix, spotlight, canUseHotkeys, isInViewport) {
+], function ($, _, ko, wrapper, events, matrix, spotlight, canUseHotkeys, isInViewport) {
     'use strict';
 
     var mouseCoords = {
@@ -266,6 +267,18 @@ define([
             target.prototype.initListeners,
             function () {
                 this.createSpotlight();
+
+                this.panel.menuSections.subscribe(_.debounce(items => {
+                    this.spotlight.spotlight('items', items.flatMap(menu => {
+                        return menu.contentTypes().map(type => {
+                            return {
+                                id: type.config.name,
+                                label: type.label(),
+                                icon: type.icon(),
+                            };
+                        })
+                    }));
+                }, 500));
 
                 this.isFullScreen.subscribe(flag => {
                     if (!flag) {
