13f2f460bead0df252b57a90f927b404f5973560
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2026-01-26 23:12:15 +0200

    WIP

--- view/adminhtml/web/js/wysiwyg/tinymceAdapter-mixin.js
+++ view/adminhtml/web/js/wysiwyg/tinymceAdapter-mixin.js
@@ -1,8 +1,26 @@
 define([
+    'jquery',
     'tinymce',
-    'mage/utils/wrapper'
-], (tinyMCE, wrapper) => {
+    'underscore',
+    'mage/utils/wrapper',
+    'Magento_PageBuilder/js/events'
+], ($, tinyMCE, _, wrapper, events) => {
     'use strict';
+
+    events.on('stage:fullScreenModeChangeAfter', (data) => {
+        if (data.fullScreen) {
+            $('.pagebuilder-stage-wrapper.stage-is-active')
+                .get(0)
+                .addEventListener('scroll', (e) => {
+                    var scrollTop = e.target.scrollTop;
+
+                    console.log(
+                        scrollTop,
+                        $('.tox-tinymce-inline:not([style*="display: none"])').offset()
+                    );
+                }, { passive: true });
+        }
+    });
 
     return function (target) {
         target.getSettings = wrapper.wrap(target.getSettings, function(o) {
