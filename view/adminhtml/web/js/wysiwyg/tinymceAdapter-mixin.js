f75d54487d72368d759181668133d7d986c5d9e5
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2026-01-29 00:01:51 +0200

    WIP

--- view/adminhtml/web/js/wysiwyg/tinymceAdapter-mixin.js
+++ view/adminhtml/web/js/wysiwyg/tinymceAdapter-mixin.js
@@ -7,36 +7,61 @@ define([
 ], ($, tinyMCE, _, wrapper, events) => {
     'use strict';
 
-    var curStage;
+    var stickyToolbar = (() => {
+        var minTop = 50,
+            waitTimer,
+            defaultTop,
+            toolbar;
 
-    function onScroll() {
-        const toolbar = document.querySelector('.tox-tinymce-inline:not([style*="display: none"])');
+        function start() {
+            toolbar = $('.tox-tinymce-inline:not([style*="display: none"])')[0];
 
-        if (!toolbar) {
-            return;
+            if (toolbar) {
+                defaultTop = toolbar.offsetTop;
+                return updatePosition();
+            }
+
+            waitTimer = setTimeout(start, 100);
         }
 
-        const rect = toolbar.getBoundingClientRect();
-        const minTop = 50;
-        const maxBottom = window.innerHeight;
-
-        let delta = 0;
-
-        if (rect.top < minTop) {
-            delta = minTop - rect.top;
-        } else if (rect.bottom > maxBottom) {
-            delta = maxBottom - rect.bottom;
+        function stop() {
+            clearTimeout(waitTimer);
+            toolbar = null;
         }
 
-        if (delta !== 0) {
-            toolbar.style.top = (toolbar.offsetTop + delta) + 'px';
-        }
-    }
+        function updatePosition() {
+            if (!toolbar) {
+                return;
+            }
 
-    document.addEventListener('scroll', onScroll, {
-        capture: true,
-        passive: true
-    });
+            const rect = toolbar.getBoundingClientRect();
+            const maxBottom = window.innerHeight;
+
+            let delta = 0;
+
+            if (rect.top < minTop) {
+                delta = minTop - rect.top;
+            } else if (rect.bottom > maxBottom) {
+                delta = maxBottom - rect.bottom;
+            } else if (defaultTop < 0 && rect.top > 1 && toolbar.offsetTop > defaultTop) {
+                delta = minTop - rect.top;
+            } else if (defaultTop > 0 && rect.bottom < maxBottom && toolbar.offsetTop < defaultTop) {
+                delta = maxBottom - rect.bottom;
+            }
+
+            toolbar.style.top = toolbar.offsetTop + delta + 'px';
+        }
+
+        document.addEventListener('scroll', updatePosition, {
+            capture: true,
+            passive: true
+        });
+
+        return {
+            start,
+            stop,
+        };
+    })();
 
     return function (target) {
         target.getSettings = wrapper.wrap(target.getSettings, function(o) {
@@ -48,6 +73,13 @@ define([
             if (settings.toolbar && !settings.toolbar.includes(' code')) {
                 settings.toolbar += ' | code';
             }
+
+            settings.setup = wrapper.wrap(settings.setup, (o, editor) => {
+                o(editor);
+
+                editor.on('focus', () => setTimeout(stickyToolbar.start, 200));
+                editor.on('blur', stickyToolbar.stop);
+            });
 
             return settings;
         });
