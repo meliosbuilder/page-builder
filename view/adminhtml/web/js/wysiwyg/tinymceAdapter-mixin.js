e9d961a99114744813acf09706753d15025b75cc
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2026-04-20 22:20:35 +0300

    Do not strip svg from the wysiwyg value

--- view/adminhtml/web/js/wysiwyg/tinymceAdapter-mixin.js
+++ view/adminhtml/web/js/wysiwyg/tinymceAdapter-mixin.js
@@ -4,6 +4,21 @@ define([
 ], (tinyMCE, wrapper) => {
     'use strict';
 
+    function addSvgSupport(settings) {
+        var svgElements = [
+            'svg,defs,pattern,desc,metadata,g,mask,path',
+            'line,marker,rect,circle,ellipse,polygon,polyline',
+            'linearGradient,radialGradient,stop,image,view,text',
+            'textPath,title,tspan,glyph,symbol,switch,use',
+        ].join(',')
+
+        settings.extended_valid_elements = settings.extended_valid_elements || '';
+        if (settings.extended_valid_elements.length) {
+            settings.extended_valid_elements += ',';
+        }
+        settings.extended_valid_elements += svgElements.split(',').map(el => el + '[*]').join(',');
+    }
+
     return function (target) {
         target.getSettings = wrapper.wrap(target.getSettings, function(o) {
             var settings = o();
@@ -14,6 +29,8 @@ define([
             if (settings.toolbar && !settings.toolbar.includes(' code')) {
                 settings.toolbar += ' | code';
             }
+
+            addSvgSupport(settings);
 
             return settings;
         });
