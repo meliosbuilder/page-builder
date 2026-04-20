ef7aa2c991d5bae06c1fc8b38105a8f4a870e956
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2026-04-20 17:00:35 +0300

    WIP

--- view/adminhtml/web/js/instant-preview/top-modal-utils.js
+++ view/adminhtml/web/js/instant-preview/top-modal-utils.js
@@ -5,6 +5,8 @@ define([
 ], function ($, _, ko) {
     'use strict';
 
+    var allowUpdateModalSource = true;
+
     function topModal() {
         return $('.modals-wrapper > ._show')
             .sort((a, b) => b.style.zIndex - a.style.zIndex)
@@ -19,21 +21,33 @@ define([
         updateSource: _.throttle(source => {
             var topSource = ko.dataFor(topModal().find('[name="appearance"]')[0])?.source;
 
-            if (topSource?.name === source?.name) {
+            if (allowUpdateModalSource && topSource?.name === source?.name) {
                 source?.save?.();
             }
         }, 80),
 
         updateModalData: _.throttle(data => {
-            var topSource = ko.dataFor(topModal().find('[name="appearance"]')[0])?.source;
+            var modal = topModal(),
+                topSource = ko.dataFor(modal.find('[name="appearance"]')[0])?.source;
 
             if (!topSource) {
                 return;
             }
 
+            setTimeout(() => allowUpdateModalSource = true, 100);
             _.each(data, (v, k) => {
                 if (typeof v === 'string' && topSource.data[k] !== v) {
+                    var el = ko.dataFor(modal.find(`[name="${k}"]`).closest('[data-bind]')[0]);
+
+                    allowUpdateModalSource = false;
                     topSource.set(`data.${k}`, v);
+
+                    if (el.wysiwygId &&
+                        tinyMCE.get(el.wysiwygId) &&
+                        tinyMCE.get(el.wysiwygId).getContent() !== v
+                    ) {
+                        tinyMCE.get(el.wysiwygId).setContent(v);
+                    }
                 }
             });
         }, 80),
